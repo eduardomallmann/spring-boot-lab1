86fc26f8960674f24bcee1507aafbecf51d75d63
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-06-20 10:22:40 -0300

    Add initial project complete

---
+++ src/main/java/com/voffice/treinamento/springbootlab1/SpringBootLab1Application.java
@@ -0,0 +1,12 @@
+package com.voffice.treinamento.springbootlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLab1Application.class, args);
+	}
+}
